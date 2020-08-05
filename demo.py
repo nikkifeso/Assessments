bdd4ea0aa1497334df32dd63e4b6a93b21f0d53a
Adenike Awofeso <adenikeawofeso@gmail.com>
2020-08-05 11:54:23 +0100

    Created function isum and created class DemoClass

---
+++ demo.py
@@ -0,0 +1,13 @@
+def isum(a,b):
+    if a == 5:
+        if b == 10:
+            isum_result = a + b
+    return isum_result
+
+def DemoClass(self, name):
+    def __init__(name):
+        self.name = name
+    def getName(self):
+        print(self.name)
+
+
